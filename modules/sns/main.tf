207192109d714c55e4f5c2aa81371d94c339653c
niyaz-hasan <60500585+niyaz-hasan@users.noreply.github.com>
2025-06-04 15:47:00 +0530

    added mail

--- modules/sns/main.tf
+++ modules/sns/main.tf
@@ -6,5 +6,5 @@ resource "aws_sns_topic" "image_processing_topic" {
 resource "aws_sns_topic_subscription" "email_subscription" {
   topic_arn = aws_sns_topic.image_processing_topic.arn
   protocol  = "email"
-  endpoint  = "stakeholder@example.com"
+  endpoint  = "niyaz.hasanmohamed@hcltech.com"
 }
