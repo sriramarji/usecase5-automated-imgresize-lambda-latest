0e628e29ea868cdb5808fc949867e8bfe1d839d0
niyaz-hasan <60500585+niyaz-hasan@users.noreply.github.com>
2025-06-04 23:14:57 +0530

    add changes

--- modules/lambda/variables.tf
+++ modules/lambda/variables.tf
@@ -1,6 +1,7 @@
-variable "lambda_function_name" {}
-variable "lambda_code_bucket" {}
-variable "lambda_code_key" {}
+variable "lambda_function_name" {
+    description "lambda function name"
+    type = string
+}
 variable "image_bucket_name" {}
 variable "image_bucket_arn" {}
 variable "destination_bucket_name" {}
