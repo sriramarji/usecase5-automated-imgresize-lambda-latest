7107df9b498f6298a448d125caffd618acc87947
niyaz-hasan <60500585+niyaz-hasan@users.noreply.github.com>
2025-06-04 23:24:51 +0530

    added fix

--- modules/lambda/variables.tf
+++ modules/lambda/variables.tf
@@ -2,7 +2,7 @@ variable "lambda_function_name" {
     description = "lambda function name"
     type = string
 }
-variable "image_bucket_name" {}
-variable "image_bucket_arn" {}
+variable "source_path" {}
+variable "s3_bucket_arn" {}
 variable "destination_bucket_name" {}
 variable "sns_topic_arn" {}
