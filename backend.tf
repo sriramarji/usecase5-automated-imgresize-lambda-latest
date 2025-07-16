55a2472441d19ed6fcba8f860d6e32c2fc862f8b
sriramarji <52248143+sriramarji@users.noreply.github.com>
2025-07-16 18:06:55 +0530

    Updated backend.tf

--- backend.tf
+++ backend.tf
@@ -1,10 +1,9 @@
-  terraform {
+terraform {
   backend "s3" {
-    bucket = "terraform-remote-st"
-    key    = "lambda-image/terraform.tfstate"
-    region = "us-east-1" 
- 
-    # For State Locking
-    dynamodb_table = "terraform-locks"    
-  } 
-  } 
+    bucket       = "tf-bucket-prod1"
+    key          = "uc5-lambda/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+    encrypt      = true
+  }
+}
